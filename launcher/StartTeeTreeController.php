9d2d2620b61bc42a23723d9f0710dcc30639230d
Andy Boxer <andrew.boxer@anobii.com>
2012-08-02 00:58:10 +0100

    stop message

--- launcher/StartTeeTreeController.php
+++ launcher/StartTeeTreeController.php
@@ -17,7 +17,7 @@ require_once("/home/webapps/TeeTree/config/TeeTreeBootStrap.php");
 if(isset($argv[1]) && $argv[1] === "start")
 {
     // Now we can get started and kick off the TeeTree Controller process
-    print("controller starting ...\n");
+    print("TeeTree controller starting ...\n");
     // We start the service controller passing a port nunber and the class path to our services classes ( This is strictly one directory intentionally and must be r/o )
     TeeTreeController::startServer(TeeTreeConfiguration::DEFAULT_SERVICE_PATH, TeeTreeConfiguration::TEETREE_SERVER_PORT);
     // This process will now continue untill the TeeTreeController::stopServer call is made using the same port no.
@@ -26,5 +26,6 @@ else
 {
     // So we want to bail, kill the server
     TeeTreeController::stopServer('localhost', TeeTreeConfiguration::TEETREE_SERVER_PORT);
+    print("TeeTree controller stopped ...\n");
 }
 ?>
